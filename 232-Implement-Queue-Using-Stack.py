a8b2f2e6901c3967d89b657213028727c0b008a1
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-09-14 17:26:10 +0530

    Update 232-Implement-Queue-Using-Stack.py

--- 232-Implement-Queue-Using-Stack.py
+++ 232-Implement-Queue-Using-Stack.py
@@ -1,1 +1,41 @@
+class MyQueue {
+    private:
+        std::vector<int> s1, s2;
 
+    public:
+        MyQueue() {}
+
+        // Push element x to the back of queue.
+        void push(int x) {
+            s1.push_back(x);
+        }
+
+        // Removes the element from the front of queue and returns that element.
+        int pop() {
+            if (s2.empty()) {
+                while (!s1.empty()) {
+                    s2.push_back(s1.back());
+                    s1.pop_back();
+                }
+            }
+            int val = s2.back();
+            s2.pop_back();
+            return val;
+        }
+
+        // Get the front element.
+        int peek() {
+            if (s2.empty()) {
+                while (!s1.empty()) {
+                    s2.push_back(s1.back());
+                    s1.pop_back();
+                }
+            }
+            return s2.back();
+        }
+
+        // Returns whether the queue is empty.
+        bool empty() {
+            return s1.empty() && s2.empty();
+        }
+    };
