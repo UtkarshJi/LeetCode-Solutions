8ba3ac3560eaf6f2da8d60e5b8b1f8df9364a170
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-09-17 10:45:23 +0530

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> numToIndexMap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+
+            if (numToIndexMap.find(diff) != numToIndexMap.end()) {
+                return {i, numToIndexMap[diff]};
+            }
+
+            numToIndexMap[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
