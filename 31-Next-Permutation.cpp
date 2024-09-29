5278fcae5b1353db6fdf3f83de7409ba043b80bc
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-09-29 12:06:56 +0530

    Update 31-Next-Permutation.cpp

--- 31-Next-Permutation.cpp
+++ 31-Next-Permutation.cpp
@@ -4,22 +4,18 @@ public:
         int n = nums.size();
         int i = n - 2;
 
-        // Step 1: Find the first decreasing element from the right
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
         if (i >= 0) {
-            // Step 2: Find the element just larger than nums[i]
             int j = n - 1;
             while (nums[j] <= nums[i]) {
                 j--;
             }
-            // Step 3: Swap nums[i] with nums[j]
             swap(nums[i], nums[j]);
         }
 
-        // Step 4: Reverse the suffix starting from nums[i+1]
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
