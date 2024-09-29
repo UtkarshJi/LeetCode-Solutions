4fac23e125273f096cb9345f9c67f5f49e29bcf8
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-09-29 12:20:50 +0530

    Update 53-Maximum-Subarray.cpp

--- 53-Maximum-Subarray.cpp
+++ 53-Maximum-Subarray.cpp
@@ -3,7 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxSum = nums[0], currentSum = nums[0];
 
-        for (size_t i = 1; i < nums.size(); i++) {
+        for (int i = 1; i < nums.size(); i++) {
             currentSum = (nums[i] > currentSum + nums[i]) ? nums[i] : currentSum + nums[i];
             maxSum = (currentSum > maxSum) ? currentSum : maxSum;
         }
