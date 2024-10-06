0c08854a1bbc56ed00c461c17c4ba03b3a4ac23c
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-06 23:04:31 +0530

    Update 75-Sort-Colors.cpp

--- 75-Sort-Colors.cpp
+++ 75-Sort-Colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int> &nums){
     int n = nums.size(); 
-    
+     
     for(int i=0; i<n-1; i++){
         int min_idx = i;
         for(int j=i+1; j<n; j++){
