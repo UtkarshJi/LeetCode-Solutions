33780b6490eb96a078f60f9cd047ebb2bd226d50
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-07 22:29:37 +0530

    Create 2696-Minimum-String-Length-After-Removing-Substrings.cpp

---
+++ 2696-Minimum-String-Length-After-Removing-Substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            if(s[i]=='B' && !st.empty() && st.top()=='A'){
+                st.pop();
+            }
+            else if(s[i]=='D' && !st.empty() && st.top()=='C'){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+    }
+    return st.size();
+    }
+};
