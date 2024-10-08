025072942f9fdb80160339baf22946f4e18e1540
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-08 22:51:46 +0530

    Update 48-Rotate-Image.cpp

--- 48-Rotate-Image.cpp
+++ 48-Rotate-Image.cpp
@@ -6,7 +6,7 @@ public:
             for(int j=i+1; j<n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
-        }
+        } 
         for(int i=0; i<n; i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
