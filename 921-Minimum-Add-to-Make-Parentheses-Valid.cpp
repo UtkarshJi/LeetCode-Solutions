e89848b6f3b52d69f9c76309875a50f98a6b5dec
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-09 23:00:01 +0530

    Update 921-Minimum-Add-to-Make-Parentheses-Valid.cpp

--- 921-Minimum-Add-to-Make-Parentheses-Valid.cpp
+++ 921-Minimum-Add-to-Make-Parentheses-Valid.cpp
@@ -3,7 +3,7 @@ public:
     int minAddToMakeValid(string s) {
         int open = 0;
         int close = 0;
-        
+         
         for(char c : s) {
             if(c == '(') {
                 close++;
