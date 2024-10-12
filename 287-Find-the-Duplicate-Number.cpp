c9a7cbbb13db0e6bcdfa938b0f0788379dd367c1
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-12 19:00:11 +0530

    Update 287-Find-the-Duplicate-Number.cpp

--- 287-Find-the-Duplicate-Number.cpp
+++ 287-Find-the-Duplicate-Number.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int sameNum = 0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++){
+            if(nums[i]==nums[i+1]){
+                sameNum = nums[i];
+                break;
+            }
+        }
+    return sameNum;
+    }
+};
