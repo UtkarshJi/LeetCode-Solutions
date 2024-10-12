5523e35fa0354e620294cda8143ddbf2be5e58ba
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-12 18:45:34 +0530

    Update 88-Merge-Sorted-Array.cpp

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int left = m-1;
+        int right = n-1;
+        int merged = m+n-1;
 
+        while(left >= 0 && right >= 0){
+            if(nums1[left] > nums2[right]){
+                nums1[merged--] = nums1[left--];
+            }
+            else{
+                nums1[merged--] = nums2[right--];
+            }
+        }
+        while(right >= 0){
+            nums1[merged--] = nums2[right--];
+        }
+    }
+};
