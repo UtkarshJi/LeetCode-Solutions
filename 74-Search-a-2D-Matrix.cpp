f9ea4b0e82628da05faebec5843e34934b323dd8
Utkarsh Sharma <149104418+UtkarshJi@users.noreply.github.com>
2024-10-13 12:13:13 +0530

    Create 74-Search-a-2D-Matrix.cpp

---
+++ 74-Search-a-2D-Matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+    return false;
+    }
+};
